52f4b069b2a0fcc607c070179eea92ed976d620f
youtheyeon <yuseoyeon@ewhain.net>
2023-12-27 16:11:35 +0900

    refactor: 태그 추가 요청 모달 수정

--- src/components/mapmaking/FeedBackModal.jsx
+++ src/components/mapmaking/FeedBackModal.jsx
@@ -10,85 +10,67 @@ const FeedBackModal = ({ onClose }) => {
   };
 
   return (
-    <Wrapper>
-      {isSubmitted ? (
-        // 제출 완료 후 보여질 새로운 화면
-        <>
-          <Box1>내친만에 직접 작성해주신 피드백을 전송했어요</Box1>
-          <Box2>검토 후 빠른 시일 내에 반영하도록 하겠습니다 ^^</Box2>
-          <OkBtn onClick={onClose}>Ok</OkBtn>
-        </>
-      ) : (
-        <>
-          <Box3>원하는 태그를 찾지 못했나요?</Box3>
-          <Box4>내친만에 직접 원하는 태그 추가 요청을 피드백 해보세요!</Box4>
-          <Container1>
+    <>
+      <Overlay />
+      <Wrapper>
+        {isSubmitted ? (
+          // 제출 완료 후 보여질 새로운 화면
+          <>
+            <YellowBox style={{ transform: "rotate(15deg)" }}>내친만에 직접 작성해주신 피드백을 전송했어요</YellowBox>
+            <YellowBox style={{ transform: "rotate(-15deg)" }}>
+              검토 후 빠른 시일 내에 반영하도록 하겠습니다 ^^
+            </YellowBox>
+            <YellowBox onClick={onClose} style={{ marginTop: "150px" }}>
+              Ok
+            </YellowBox>
+          </>
+        ) : (
+          <>
+            <GrayBox style={{ transform: "rotate(15deg)" }}>원하는 태그를 찾지 못했나요?</GrayBox>
+            <GrayBox style={{ marginTop: "70px" }}>내친만에 직접 원하는 태그 추가 요청을 피드백 해보세요!</GrayBox>
+
             <InputBox placeholder="요기에 입력 !" />
-            <Container2>
-              <SubmitBtn onClick={handleSubmit}>Submit</SubmitBtn>
-              <BackBtn onClick={onClose}>Back</BackBtn>
-            </Container2>
-          </Container1>
-        </>
-      )}
-    </Wrapper>
+            <BtnContainer>
+              <Btn onClick={handleSubmit} style={{ background: "var(--yellow)" }}>
+                Submit
+              </Btn>
+              <Btn onClick={onClose} style={{ background: "var(--gray)" }}>
+                Back
+              </Btn>
+            </BtnContainer>
+          </>
+        )}
+      </Wrapper>
+    </>
   );
 };
 
 export default FeedBackModal;
 
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.8);
+  z-index: 1;
+`;
+
 const Wrapper = styled.div`
   position: fixed;
+  z-index: 2;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100%;
-  z-index: 10;
 `;
 
-const Box1 = styled.div`
+const YellowBox = styled.div`
   margin-top: 125px;
-  width: 100%;
-  height: 61px;
-  transform: rotate(15deg);
-  flex-shrink: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1.5px solid var(--black1);
-  background: var(--yellow);
-  color: var(--Background_color, var(--black3));
-  text-align: center;
-  font-family: Apple SD Gothic Neo;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 145%;
-  letter-spacing: 1.4px;
-`;
-const Box2 = styled.div`
-  margin-top: 125px;
-  width: 100%;
-  height: 61px;
-  transform: rotate(-15deg);
-  flex-shrink: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1.5px solid var(--black1);
-  background: var(--yellow);
-  color: var(--Background_color, var(--black3));
-  text-align: center;
-  font-family: Apple SD Gothic Neo;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 145%;
-  letter-spacing: 1.4px;
-`;
-const OkBtn = styled.div`
-  margin-top: 150px;
-  width: 100%;
+  width: 110vw;
   height: 61px;
   flex-shrink: 0;
   display: flex;
@@ -96,114 +78,76 @@ const OkBtn = styled.div`
   align-items: center;
   border: 1.5px solid var(--black1);
   background: var(--yellow);
-  color: var(--Background_color, var(--black3));
+
+  color: var(--black3);
   text-align: center;
   font-family: Apple SD Gothic Neo;
   font-size: 14px;
-  font-style: normal;
   font-weight: 600;
   line-height: 145%;
   letter-spacing: 1.4px;
-  cursor: pointer;
 `;
-const Box3 = styled.div`
+
+const GrayBox = styled.div`
   margin-top: 125px;
-  width: 100%;
+  width: 110vw;
   height: 61px;
-  transform: rotate(15deg);
   flex-shrink: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   border: 1.5px solid var(--black1);
-  background: #d9d9d9;
-  color: var(--Background_color, var(--black3));
+  background: var(--gray);
+
+  color: var(--black3);
   text-align: center;
   font-family: Apple SD Gothic Neo;
   font-size: 14px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 145%;
-  letter-spacing: 1.4px;
-`;
-const Box4 = styled.div`
-  margin-top: 70px; //차후 수정 필요
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 61px;
-  flex-shrink: 0;
-  border: 1.5px solid var(--black1);
-  background: #d9d9d9;
-  color: var(--Background_color, var(--black3));
-  text-align: center;
-  font-family: Apple SD Gothic Neo;
-  font-size: 14px;
-  font-style: normal;
   font-weight: 600;
   line-height: 145%;
   letter-spacing: 1.4px;
 `;
 
-const Container1 = styled.div`
-  margin: auto;
-`;
-
-const InputBox = styled.input`
+const InputBox = styled.textarea`
   margin-top: 31px;
+  padding: 18px 22px;
+  box-sizing: border-box;
   width: 456px;
   height: 199px;
   border: 3px solid var(--yellow);
   background-color: var(--white);
-  color: rgba(0, 0, 0, 0.3);
+
+  color: var(--black1);
   font-family: "Hack Regular";
   font-size: 14.961px;
-  font-style: normal;
   font-weight: 400;
   line-height: 145%;
   letter-spacing: 1.496px;
+
+  ::placeholder {
+    color: rgba(0, 0, 0, 0.3);
+  }
 `;
 
-const Container2 = styled.div`
+const BtnContainer = styled.div`
+  width: 456px;
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  justify-content: center;
 `;
 
-const SubmitBtn = styled.div`
+const Btn = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 122px;
   height: 38px;
   flex-shrink: 0;
-  background-color: var(--yellow);
-  color: var(--black1);
-  text-align: center;
-  font-family: "Hack Regular";
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  cursor: pointer;
-`;
 
-const BackBtn = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 122px;
-  height: 38px;
-  flex-shrink: 0;
-  background-color: #d9d9d9;
   color: var(--black1);
   text-align: center;
   font-family: "Hack Regular";
   font-size: 14px;
-  font-style: normal;
   font-weight: 700;
-  line-height: normal;
   cursor: pointer;
 `;
--- src/pages/mapmaking/ThemePage.jsx
+++ src/pages/mapmaking/ThemePage.jsx
@@ -2,12 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import TopBar from "../../components/_common/TopBar";
-import {
-  Line1,
-  Line2,
-  WhiteBox,
-  NextBtnBlack,
-} from "../../components/_common/CommonExport";
+import { Line1, WhiteBox, NextBtnBlack } from "../../components/_common/CommonExport";
 import FeedBackModal from "../../components/mapmaking/FeedBackModal";
 
 const ThemePage = () => {
@@ -21,16 +16,7 @@ const ThemePage = () => {
     setIsModalOpen(false);
   };
 
-  const Themes = [
-    "#맛집",
-    "#명소",
-    "#카페",
-    "#자연",
-    "#산책",
-    "#빵",
-    "#국밥",
-    "#브런치",
-  ];
+  const Themes = ["#맛집", "#명소", "#카페", "#자연", "#산책", "#빵", "#국밥", "#브런치"];
 
   return (
     <Wrapper>
@@ -44,12 +30,7 @@ const ThemePage = () => {
       </Content>
       <FeedbackBtn onClick={handleOpanModal}>어 뭐야 왜없어??</FeedbackBtn>
       <NextBtnBlack where={"/mapmaking/name"} />
-      {isModalOpen && (
-        <>
-          <Overlay />
-          <FeedBackModal onClose={handleCloseModal} />
-        </>
-      )}
+      {isModalOpen && <FeedBackModal onClose={handleCloseModal} />}
     </Wrapper>
   );
 };
@@ -88,7 +69,7 @@ const Content = styled.div`
 const FeedbackBtn = styled.div`
   margin-top: 116px;
   margin-bottom: 80px; //임시 설정
-  width: 100%;
+  width: 110vw;
   height: 61px;
   transform: rotate(-15deg);
   flex-shrink: 0;
@@ -97,23 +78,13 @@ const FeedbackBtn = styled.div`
   align-items: center;
   border: 1.5px solid var(--black1);
   background: var(--yellow);
-  color: var(--Background_color, var(--black3));
+
+  color: var(--black3);
   text-align: center;
   font-family: Apple SD Gothic Neo;
   font-size: 14px;
-  font-style: normal;
   font-weight: 700;
   line-height: 145%;
   letter-spacing: 1.4px;
   cursor: pointer;
-`;
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.8);
-  z-index: 9;
 `;
